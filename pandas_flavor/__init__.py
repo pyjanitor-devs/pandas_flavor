0888accdbcd14f6e68e8bc9be153d948017f5fac
Eric Ma <ericmjl@users.noreply.github.com>
2022-04-17 15:51:07 +0000

    Linter formatter

--- pandas_flavor/__init__.py
+++ pandas_flavor/__init__.py
@@ -1,7 +1,7 @@
-from .register import (register_series_method,
-                       register_series_accessor,
-                       register_dataframe_method,
-                       register_dataframe_accessor)
-from .xarray import (register_xarray_dataarray_method, 
-                     register_xarray_dataset_method)
-
+from .register import (
+    register_series_method,
+    register_series_accessor,
+    register_dataframe_method,
+    register_dataframe_accessor,
+)
+from .xarray import register_xarray_dataarray_method, register_xarray_dataset_method
