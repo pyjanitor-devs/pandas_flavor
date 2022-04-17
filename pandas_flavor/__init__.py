e9bcfe0dd313ee666952adc61880d51757ebe5e4
Eric Ma <ericmjl@users.noreply.github.com>
2022-04-17 16:44:01 +0000

    Ensured that lazy loading works with dataframe registration.

--- pandas_flavor/__init__.py
+++ pandas_flavor/__init__.py
@@ -1,17 +1,37 @@
 """Top-level API for pandas-flavor."""
-from .register import (
-    register_series_method,
-    register_series_accessor,
-    register_dataframe_method,
-    register_dataframe_accessor,
-)
-from .xarray import register_xarray_dataarray_method, register_xarray_dataset_method
+import lazy_loader as lazy
 
-__all__ = [
-    "register_series_method",
-    "register_series_accessor",
-    "register_dataframe_method",
-    "register_dataframe_accessor",
-    "register_xarray_dataarray_method",
-    "register_xarray_dataset_method",
-]
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submod_attrs={
+        "register": [
+            "register_series_method",
+            "register_series_accessor",
+            "register_dataframe_method",
+            "register_dataframe_accessor",
+        ],
+        "xarray": [
+            "register_xarray_dataarray_method",
+            "register_xarray_dataset_method",
+        ],
+    },
+)
+# from .register import (
+#     register_series_method,
+#     register_series_accessor,
+#     register_dataframe_method,
+#     register_dataframe_accessor,
+# )
+# from .xarray import (
+#     register_xarray_dataarray_method,
+#     register_xarray_dataset_method,
+# )
+
+# __all__ = [
+#     "register_series_method",
+#     "register_series_accessor",
+#     "register_dataframe_method",
+#     "register_dataframe_accessor",
+#     "register_xarray_dataarray_method",
+#     "register_xarray_dataset_method",
+# ]
