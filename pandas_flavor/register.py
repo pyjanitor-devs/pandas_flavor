88c17ee60aac38b1c16bfe75a6117a563a13322d
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-01-11 11:25:41 -0500

    added method signature member to be passed to the callback handling method call

--- pandas_flavor/register.py
+++ pandas_flavor/register.py
@@ -1,6 +1,7 @@
 import ipdb
 from functools import wraps
 from pandas.api.extensions import register_series_accessor, register_dataframe_accessor
+import inspect
 from . import stack_counter
 
 start_method_call = None
@@ -19,6 +20,8 @@ def register_dataframe_method(method):
             print(df[col])
     """
 
+    method_signature = inspect.signature(method)
+
     def inner(*args, **kwargs):
         class AccessorMethod(object):
             def __init__(self, pandas_obj):
@@ -34,12 +37,13 @@ def register_dataframe_method(method):
                     stack_depth = sc.scf.level
                     if stack_depth == 1:
                         if start_method_call:
-                            method_call_obj = start_method_call(self._obj, method.__name__, args, kwargs, stack_depth)
+                            #ipdb.set_trace()
+                            method_call_obj = start_method_call(self._obj, method.__name__, method_signature, args, kwargs, stack_depth)
                         ret = method(self._obj, *args, **kwargs)
                     elif stack_depth == 2 and method.__name__ == 'copy':
                         #ipdb.set_trace()
                         if start_method_call:
-                            method_call_obj = start_method_call(self._obj, method.__name__, args, kwargs, stack_depth)
+                            method_call_obj = start_method_call(self._obj, method.__name__, method_signature, args, kwargs, stack_depth)
                         ret = method(self._obj, *args, **kwargs)
                     else:
                         ret = method(self._obj, *args, **kwargs)
@@ -59,6 +63,8 @@ def register_dataframe_method(method):
 def register_series_method(method):
     """Register a function as a method attached to the Pandas Series."""
 
+    method_signature = inspect.signature(method)
+    
     def inner(*args, **kwargs):
         class AccessorMethod(object):
             __doc__ = method.__doc__
@@ -74,7 +80,7 @@ def register_series_method(method):
                     if stack_depth <= 2:
                         global start_method_call
                         if start_method_call:
-                            method_call_obj = start_method_call(self._obj, method.__name__, args, kwargs, stack_depth)
+                            method_call_obj = start_method_call(self._obj, method.__name__, method_signature, args, kwargs, stack_depth)
                             
                     ret = method(self._obj, *args, **kwargs)
 
