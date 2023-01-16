725717219e98ae04f255e15b4701a75f7d776c43
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-01-16 13:15:16 -0500

    no need to keep call stack in pandas_flavor

--- pandas_flavor/register.py
+++ pandas_flavor/register.py
@@ -3,7 +3,6 @@ from pandas.api.extensions import register_series_accessor, register_dataframe_a
 import inspect
 from contextlib import nullcontext
 
-cb_create_call_stack_context_manager = None
 cb_notify_dataframe_method_call = None
 cb_notify_series_method_call = None
 
@@ -30,22 +29,20 @@ def register_dataframe_method(method):
 
             @wraps(method)
             def __call__(self, *args, **kwargs):
-                global cb_create_call_stack_context_manager
-                with cb_create_call_stack_context_manager(method.__name__) if cb_create_call_stack_context_manager else nullcontext() as sc:
-                    method_call_obj = None
-                    global cb_notify_dataframe_method_call
-                    if cb_notify_dataframe_method_call:
-                        method_call_obj = cb_notify_dataframe_method_call(self._obj, method.__name__, method_signature, args, kwargs)
-                        if method_call_obj:
-                            new_args, new_kwargs = method_call_obj.handle_start_method_call()
-                            args = new_args[1:]; kwargs = new_kwargs
-
-                    ret = method(self._obj, *args, **kwargs)
-
+                method_call_obj = None
+                global cb_notify_dataframe_method_call
+                if cb_notify_dataframe_method_call:
+                    method_call_obj = cb_notify_dataframe_method_call(self._obj, method.__name__, method_signature, args, kwargs)
                     if method_call_obj:
-                        method_call_obj.handle_end_method_call(ret)
-                    
-                    return ret
+                        new_args, new_kwargs = method_call_obj.handle_start_method_call()
+                        args = new_args[1:]; kwargs = new_kwargs
+
+                ret = method(self._obj, *args, **kwargs)
+
+                if method_call_obj:
+                    method_call_obj.handle_end_method_call(ret)
+
+                return ret
 
         register_dataframe_accessor(method.__name__)(AccessorMethod)
 
@@ -68,22 +65,20 @@ def register_series_method(method):
 
             @wraps(method)
             def __call__(self, *args, **kwargs):
-                global cb_create_call_stack_context_manager
-                with cb_create_call_stack_context_manager(method.__name__) if cb_create_call_stack_context_manager else nullcontext() as sc:
-                    method_call_obj = None
-                    global cb_notify_series_method_call
-                    if cb_notify_series_method_call:
-                        method_call_obj = cb_notify_series_method_call(self._obj, method.__name__, method_signature, args, kwargs)
-                        if method_call_obj:
-                            new_args, new_kwargs = method_call_obj.handle_start_method_call()
-                            args = new_args[1:]; kwargs = new_kwargs
-                            
-                    ret = method(self._obj, *args, **kwargs)
-
+                method_call_obj = None
+                global cb_notify_series_method_call
+                if cb_notify_series_method_call:
+                    method_call_obj = cb_notify_series_method_call(self._obj, method.__name__, method_signature, args, kwargs)
                     if method_call_obj:
-                        method_call_obj.handle_end_method_call(ret)
-                        
-                    return ret
+                        new_args, new_kwargs = method_call_obj.handle_start_method_call()
+                        args = new_args[1:]; kwargs = new_kwargs
+
+                ret = method(self._obj, *args, **kwargs)
+
+                if method_call_obj:
+                    method_call_obj.handle_end_method_call(ret)
+
+                return ret
 
         register_series_accessor(method.__name__)(AccessorMethod)
 
