1e59822e4cb615c81661247efd350879e50bac21
samuel.oranyeli <samuel.oranyeli@grow.inc>
2025-04-10 19:35:56 +1000

    create groupby accessor and method

--- pandas_flavor/__init__.py
+++ pandas_flavor/__init__.py
@@ -4,6 +4,8 @@ from .register import (
     register_dataframe_method,
     register_series_accessor,
     register_series_method,
+    register_groupby_accessor,
+    register_groupby_method,
 )
 from .xarray import (
     register_xarray_dataarray_method,
@@ -15,6 +17,8 @@ __all__ = [
     "register_series_accessor",
     "register_dataframe_method",
     "register_dataframe_accessor",
+    "register_groupby_accessor",
+    "register_groupby_method",
     "register_xarray_dataarray_method",
     "register_xarray_dataset_method",
 ]
--- tests/test_pandas_register.py
+++ tests/test_pandas_register.py
@@ -1,6 +1,8 @@
 """Tests for pandas series and dataframe method registration."""
+
 import pandas_flavor as pf
 import pandas as pd
+from pandas.core.groupby.generic import DataFrameGroupBy
 
 
 def test_register_dataframe_method():
@@ -39,3 +41,28 @@ def test_register_series_method():
 
     ser = pd.Series()
     ser.dummy_func()
+
+
+def test_register_groupby_method():
+    """Test register_groupby_method."""
+
+    @pf.register_groupby_method
+    def dummy_func(by: DataFrameGroupBy) -> DataFrameGroupBy:
+        """Dummy func.
+
+        Args:
+            by: A DataFrameGroupBy object.
+
+        Returns:
+            DataFrameGroupBy.
+        """
+        return by
+
+    df = pd.DataFrame(
+        {
+            "Animal": ["Falcon"],
+            "Max Speed": [380.0],
+        }
+    )
+    by = df.groupby("Animal")
+    by.dummy_func()
